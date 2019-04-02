d1c745c516ea51f66d1b71ee6071155388e8ee0c
will1213 <lovechi930609@gmail.com>
2019-04-02 15:11:37 -0600

    Client server

--- ClientController/Client.java
+++ ClientController/Client.java
@@ -7,6 +7,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 import View.GUI;
+package ClientController;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import View.GUI;
 
 public class Client {
 	private Socket theSocket;
@@ -16,39 +25,52 @@ public class Client {
 	public Client(String serverName, int port) {
 		try {
 		theSocket=new Socket(serverName,port);
-		objectIn=new ObjectInputStream(theSocket.getInputStream());
+		//objectIn=new ObjectInputStream(theSocket.getInputStream());
 		writeOut=new PrintWriter(theSocket.getOutputStream());
 		}catch(IOException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
+	public void sendString(String s) {
+		writeOut.println(s);
+		writeOut.flush();
+		
+	}
 	public void listSuppliers() {
+		sendString("1");
 		System.out.println("list suppliers");
 	}
 	public void listTools() {
+		sendString("2");
 		System.out.println("list tools");
 	}
 	public void searchName(String name) {
+		sendString("3");
 		System.out.println("Searching name: " + name);
 	}
 	public void searchID(String ID) {
+		sendString("4");
 		System.out.println("Searching id: " + ID);
 	}
 	public void check(String ID) {
+		sendString("5");
 		System.out.println("Searching id for checking stock: " + ID);
 	}
 	public void decrease(String ID, String amount) 
 	{
+		sendString("6");
 		System.out.println("Going to decrease item id: " + ID + " by this amount: " + amount);
 	}
 	public void order() {
+		
+		sendString("7");
 		System.out.println("printing orders...");		
 	}
 	public static void main(String[] args) {
 		Client c=new Client("localhost", 8099);
-		//GUI test = new GUI();
-		//test.buildAll();
+		GUI test = new GUI(c);
+		test.buildAll();
 		
 		
 	}
--- ClientController/Server.java
+++ ClientController/Server.java
@@ -1,4 +1,8 @@
+package Server;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,10 +11,11 @@ import java.util.concurrent.Executors;
 
 public class Server {
 	private ObjectOutputStream objectOut;
+	private BufferedReader socketIn;
 	private ServerSocket serverSocket;
 	private Socket theSocket;
 	private ExecutorService pool;
-	
+	//private Shop theShop;
 	public Server(int port) {
 		try {
 		serverSocket=new ServerSocket(port);
@@ -18,15 +23,59 @@ public class Server {
 			System.err.println(e.getMessage());
 		}
 		pool= Executors.newCachedThreadPool();
+		System.out.println("Server is now working!");
 	}
 	public void communicate() {
-		boolean running=true;
-		while(running) {
+		String input;
+		while(true) {
+			try {
+			theSocket=serverSocket.accept();
+			socketIn=new BufferedReader(new InputStreamReader(theSocket.getInputStream()));
+			while(true) {
 			
+			
+			input=socketIn.readLine();
+			System.out.println(input);
+			switch(input){
+				case "1":
+							System.out.println("11111");
+							break;
+				case "2":
+					System.out.println("2222");	
+							break;
+				case "3":
+					System.out.println("33333");	
+							break;
+				case "4":
+					System.out.println("44444");	
+							break;
+				case "5":
+					System.out.println("5555");	
+							break;
+				case "6":
+					System.out.println("6666");	
+							break;
+				case "7":
+					System.out.println("777");	
+							break;
+			}				
+			
+			}
+			}
+			
+			catch(IOException e) {
+				System.out.println(e.getMessage());
+			}
+			
+			
+			
+			
+			}
 		}
 		
-	}
+	
 	public static void main (String args[]) {
 		Server s= new Server(8099);
+		s.communicate();
 	}
 }
